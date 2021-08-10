96d02356216e50e87b40ba99349b99c0e7770674
Le1ns <Lagutka_98@mail.ru>
2021-08-10 22:47:12 +0300

    Input and print first part done

--- FirstLesson/Print Input.py
+++ FirstLesson/Print Input.py
@@ -3,18 +3,18 @@ print()
 print("one", 'two', "Three")
 print("i want to use 'test' for looking kovichki")
 print('i want to see "kovichki"')
-#1
+# 1
 print("Здравствуй, мир!")
-#2
+# 2
 print("4", "8", "15", "16", "23", "42")
-#3
+# 3
 print("4")
 print("8")
 print("15")
 print("16")
 print("23")
 print("42")
-#4
+# 4
 print("*")
 print("**")
 print("***")
@@ -22,8 +22,34 @@ print("****")
 print("*****")
 print("******")
 print("*******")
-#**************************
+##########################
+""" Teory
 print('how are u?')
 name = input()
 print('hello', name)
 
+############################
+name=input("What is ur name?" )
+print("my name is", name)
+"""
+# Input #1
+##
+name = input()
+print("Привет,", name)
+##2
+name = input()
+print(name, "- чемпион!")
+##3
+first = input()
+second = input()
+third = input()
+print(first)
+print(second)
+print(third)
+##4
+first = input()
+second = input()
+third = input()
+print(third)
+print(second)
+print(first)
