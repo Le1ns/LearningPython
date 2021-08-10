70086c4c075057673e68ed6fae1b87850486c4eb
Le1ns <Lagutka_98@mail.ru>
2021-08-10 17:18:06 +0300

    Some print on work

---
+++ FirstLesson/Print Input.py
@@ -0,0 +1,29 @@
+print('We are learning pyton')
+print()
+print("one", 'two', "Three")
+print("i want to use 'test' for looking kovichki")
+print('i want to see "kovichki"')
+#1
+print("Здравствуй, мир!")
+#2
+print("4", "8", "15", "16", "23", "42")
+#3
+print("4")
+print("8")
+print("15")
+print("16")
+print("23")
+print("42")
+#4
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+#**************************
+print('how are u?')
+name = input()
+print('hello', name)
+
