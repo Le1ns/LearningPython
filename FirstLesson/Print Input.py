31da88c2d772328ebcc203f9a1ee103e098787d3
Le1ns <Lagutka_98@mail.ru>
2021-08-11 17:21:41 +0300

    integers

--- FirstLesson/Print Input.py
+++ FirstLesson/Print Input.py
@@ -53,3 +53,65 @@ third = input()
 print(third)
 print(second)
 print(first)
+##
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+
+##1
+print("I", "like", "Python", sep="***")
+##2
+delim=input()
+first=input()
+second=input()
+third=input()
+print(first, second, third, sep=delim)
+##3
+name = input()
+print("Привет,", name, end="!")
+##################################
+"""
+Celochislennie znacheniya
+"""
+##1
+count = int(input())
+print(count)
+print(count + 1)
+print(count + 2)
+##2
+first = int(input())
+second = int(input())
+third = int(input())
+print(first + second + third)
+##3
+long = int(input())
+print("Объем =", long*long*long)
+print("Площадь полной поверхности =", 6*long*long)
+##4
+a = int(input())
+b = int(input())
+print(3*((a+b)**3)+275*(b**2)-127*a-41)
+##5
+chislo = int(input())
+print("Следующее за числом", chislo, "число:", chislo+1)
+print("Для числа", chislo, "предыдущее число:", chislo-1)
+##6
+first = int(input())
+second = int(input())
+third = int(input())
+four = int(input())
+
+print((first+second+third+four)*3)
+##7
+a = int(input())
+b = int(input())
+
+print(a, "+", b, "=", a + b)
+print(a, "-", b, "=", a - b)
+print(a, "*", b, "=", a * b)
