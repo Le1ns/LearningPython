89604ac69873f1e0678397df6ce15017f3df924f
Le1ns <Lagutka_98@mail.ru>
2021-08-12 09:36:44 +0300

    Exam about input and print done

---
+++ FirstLesson/Exam№1.py
@@ -0,0 +1,36 @@
+print()
+##
+_test = 5
+print(_test)
+##
+s = 13
+k = -5
+d = s + 2
+s = d
+k = 2 * s
+print(s + k + d)
+##
+a = 17 // (23 % 7)
+b = 34 % a * 5 - 29 % 4 * 3
+print(a * b)
+##
+print('*' * 17)
+print('*', '*', sep=' ' * 15)
+print('*', '*', sep=' ' * 15)
+print('*' * 17)
+##
+a = int(input())
+b = int(input())
+print("Квадрат суммы", a, "и", b, "равен", (a+b)**2)
+print("Сумма квадратов", a, "и", b, "равна", a**2 + b**2)
+
+##
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a ** b + c ** d)
+##
+number = int(input())
+print(number * 123)
+
