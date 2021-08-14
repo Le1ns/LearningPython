3fee1945760bd7d3b33ba0fe0c2442dab36f003d
Le1ns <Lagutka_98@mail.ru>
2021-08-15 01:13:58 +0300

    +3 ex done

--- ThirdLesson/Types of variables.py
+++ ThirdLesson/Types of variables.py
@@ -19,6 +19,23 @@ if a == 0:
 else:
     print(1/a)
 
-##
+## from farengheit to celsi
 temperature = float(input())
+print((5/9)*(temperature-32))
 
+##
+age = int(input())
+if age == 1:
+    print(10.5)
+elif age == 2:
+    print(21)
+else:
+    print(21 + (age - 2)*4)
+##
+number = float(input())
+number = number * 10
+print(int(number % 10))
+##
+num = float(input())
+buff = int(num)
+print(num-buff)
