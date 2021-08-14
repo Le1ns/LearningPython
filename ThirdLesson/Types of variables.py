3b87f0add2048142331fa5a374988921032e2f23
Le1ns <Lagutka_98@mail.ru>
2021-08-14 17:53:41 +0300

    3 exercises done Next  is calculate from F to C

---
+++ ThirdLesson/Types of variables.py
@@ -0,0 +1,24 @@
+##
+a = float(input())
+b = float(input())
+
+S = 0.5*a*b
+print(S)
+##
+S = float(input())
+V1 = float(input())
+V2 = float(input())
+
+
+print(S / (V1+V2))
+
+##
+a = float(input())
+if a == 0:
+    print("Обратного числа не существует")
+else:
+    print(1/a)
+
+##
+temperature = float(input())
+
